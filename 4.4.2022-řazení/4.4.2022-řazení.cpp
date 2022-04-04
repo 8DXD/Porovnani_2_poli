88137214e5ddcdf89b191f26471eae34011798c9
8DXD <kacule05@gmail.com>
2022-04-04 14:13:10 +0200

    Upravení vzhledu

--- 4.4.2022-řazení/4.4.2022-řazení.cpp
+++ 4.4.2022-řazení/4.4.2022-řazení.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 //Vstupem jsou 2 pole čísel. Porovnejte pole (výskyt v prvním, druhém, obou)
+
 int porovnej(const void* a, const void* b)
 {
 	return (*(int*)a - *(int*)b);
@@ -13,32 +14,26 @@ int main() {
 
 	//Výpis pole
 	printf("pole1: ");
-	for (int i = 0; i < 5; i++)
-	{
+	for (int i = 0; i < 5; i++) {
 		printf("%d ", pole1[i]);
 	}
 	printf("\npole2: ");
-	for (int i = 0; i < 5; i++)
-	{
+	for (int i = 0; i < 5; i++) {
 		printf("%d ", pole2[i]);
 	}
 	printf("\n\n");
 
-	qsort(pole1,5,sizeof(int), porovnej);
+	qsort(pole1,5,sizeof(*pole1), porovnej);
+	qsort(pole2, 5, sizeof(*pole2), porovnej);
 
 	//Výpis pole
 	printf("pole1: ");
-	for (int i = 0; i < 5; i++)
-	{
+	for (int i = 0; i < 5; i++) {
 		printf("%d ", pole1[i]);
 	}
 	printf("\npole2: ");
-	for (int i = 0; i < 5; i++)
-	{
+	for (int i = 0; i < 5; i++) {
 		printf("%d ", pole2[i]);
 	}
 	printf("\n\n");
-
-	
-
 }
